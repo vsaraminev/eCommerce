cc66eabb8e2c8b7380487afa791d765d37400666
ventsi <v.saraminev@abv.bg>
2021-08-29 14:09:01 +0300

    added shopping cart

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,19 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
 import { listProductDetails } from '../actions/productActions';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
+    const [qty, setQty] = useState(1);
     const dispatch = useDispatch();
     const productDetails = useSelector(state => state.productDetails);
     const { loading, error, product } = productDetails;
     useEffect(() => {
         dispatch(listProductDetails(match.params.id))
     },[dispatch, match]);
+
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`);
+    }
 
     return (
         <>
@@ -71,8 +76,25 @@ const ProductScreen = ({ match }) => {
                                                 </Col>
                                             </Row>
                                         </ListGroup.Item>
+                                        {product.countInStock > 0 && (
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Qty</Col>
+                                                    <Col>
+                                                        <Form.Control as="select" value={qty} onChange={(e) => setQty(e.target.value)}>
+                                                            {
+                                                                [...Array(product.countInStock).keys()].map(index => (
+                                                                    <option key={index + 1} value={index + 1}>{index + 1}</option>
+                                                                ))
+                                                            }
+                                                        </Form.Control>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                        )}
                                         <ListGroup.Item>
-                                            <Button 
+                                            <Button
+                                                onClick={addToCartHandler} 
                                                 className="btn-block" 
                                                 type="button" 
                                                 disabled={product.countInStock === 0 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,23 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from "./reducers/productReducers";
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart : cartReducer
 });
 
-const initialState = {};
+const cartItemsFromStore = localStorage.getItem("cartItems") 
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [];
+
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromStore
+    }
+};
 
 const middleware = [thunk];
 
