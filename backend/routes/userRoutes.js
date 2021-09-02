a100dad88f639f468034ad47db58207cffcd948c
ventsi <v.saraminev@abv.bg>
2021-09-02 22:07:55 +0300

    User auth and profile section

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authUser, registerUser, getUserProfile } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/login', authUser);
 router.post('/', registerUser);
 router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile); 
 
 export default router;
